78bf4c0dd76f6facb4d300adab1fa712b76e0a97
Tony David <antoniod_98@hotmail.com>
2019-11-23 10:57:45 -0600

    IDE cahnges

--- IDE/js.js
+++ IDE/js.js
@@ -5,6 +5,9 @@ GlobalVectors = {}
 MainVars = {}
 MainVectors = {}
 
+paramCounter = 0;
+is_inside = "";
+
 /* -------- Variables para agregar en el html -------- */
 
 function addGlobalVar(variable){
@@ -26,6 +29,9 @@ function editGlobalVar(variable){
 }
 
 function addFunction(func_name){
+  var conditionCount = 0;
+  var loopCount = 0;
+
   var buttonText;
   var divFuncHTML = document.createElement("div");
   var divVariablesHTML = document.createElement("div")
@@ -91,7 +97,16 @@ function addFunction(func_name){
   var divUnifyHTML = document.createElement("div");
   divUnifyHTML.classList.add("is-function");
 
-  var varHtml = document.createTextNode(GlobalFuncs[func_name].header);
+  headerText = GlobalFuncs[func_name].header + "(";
+  params = GlobalFuncs[func_name].params
+
+  for (i in params){
+    headerText += params[i];
+  }
+
+  headerText += ")";
+
+  var varHtml = document.createTextNode(headerText);
   divFuncHTML.appendChild(varHtml)
 
   divUnifyHTML.appendChild(divFuncHTML);
@@ -104,15 +119,26 @@ function addFunction(func_name){
 
 function editFunction(func_name){
   var editVar = document.getElementById("name" + func_name)
-  editVar.innerHTML = GlobalFuncs[func_name].header
+  editVar.innerHTML = GlobalFuncs[func_name].header + "("
+  params = GlobalFuncs[func_name].params
+
+  for (i in params){
+    editVar.innerHTML += params[i];
+  }
+
+  editVar.innerHTML += ")";
   hideModal();
   return;
+}
+
+function addParams(func_name, param){
+
 }
 
 function addLocalVar(func_name, varname){
   var divVarHTML = document.createElement("div");
   divVarHTML.classList.add("is-variable");
-  divVarHTML.setAttribute("id", varname)
+  divVarHTML.setAttribute("id", func_name + ">" + varname)
   var varHtml = document.createTextNode(GlobalFuncs[func_name][varname]);
   divVarHTML.appendChild(varHtml);
   document.getElementById(func_name).appendChild(divVarHTML);
@@ -121,7 +147,7 @@ function addLocalVar(func_name, varname){
 }
 
 function editLocalVar(func_name, variable){
-  var editVar = document.getElementById(func_name).querySelector("#" + variable);
+  var editVar = document.getElementById(func_name).querySelector("#" + func_name + ">" + variable);
   editVar.innerHTML = GlobalFuncs[func_name][variable]
   hideModal();
   return ;
@@ -149,9 +175,7 @@ function addReturn(id){
 
 /* -------- Funciones para crear globales en el codigo y run -------- */
 function execute(){
-  for (i in GlobalVars){
-    console.log(GlobalVars[i]);
-  }
+  console.log(GlobalVars ,GlobalFuncs);
 }
 
 function createGlobalVariable(){
@@ -202,11 +226,47 @@ function createFunction(){
   function_var += functionName
   if(GlobalFuncs[functionName] != undefined){
     GlobalFuncs[functionName].header = function_var
+    GlobalFuncs[functionName].params = {}
+    GlobalFuncs[functionName].oper = {}
+    createParams(functionName)
     editFunction(functionName);
     return;
   }
   GlobalFuncs[functionName] = {header: function_var}
+  GlobalFuncs[functionName].params = {}
+  GlobalFuncs[functionName].oper = {}
+  createParams(functionName)
   addFunction(functionName)
+  return ;
+}
+
+function createParams(func_name){
+  param_var = ""
+  for(i = 1; i <= paramCounter; i++){
+    if(document.getElementById("paramInt" + i).checked == true){
+    param_var = "int "
+    }
+    if(document.getElementById("paramFloat" + i).checked == true){
+      param_var = "float "
+    }
+    if(document.getElementById("paramBool" + i).checked == true){
+      param_var = "bool "
+    }
+    if(document.getElementById("paramString" + i).checked == true){
+      param_var = "string "
+    }
+    param_Name = document.getElementById("paramName" + i).value;
+    param_var += param_Name;
+    if(i != paramCounter){
+      param_var += " ,"
+    }
+    if(GlobalFuncs[func_name].params[param_Name] != undefined){
+      GlobalFuncs[func_name].params[param_Name] = param_var
+      //editParam(func_name, param_Name)
+    }
+    GlobalFuncs[func_name].params[param_Name] = param_var
+    //addLocalVar(func_name, param_Name);
+  }
   return ;
 }
 
@@ -237,8 +297,8 @@ function createLocalVar(func_name){
   return ;
 }
 
-function createLocalOperator(func_name){
-
+function createLocalOperator(count){
+  $('<div id="' + is_inside + ">op" + count +'" class="is-operator"></div>').appendTo("#" + is_inside)
 }
 
 function createLocalVector(func_name){
@@ -249,6 +309,9 @@ function createLocalVector(func_name){
 /* -------- Funciones de modales -------- */
 
 function hideModal(){
+  paramCounter = 0
+  is_inside = ""
+  console.log("Se activo hideModal");
   var modal = document.querySelector('.is-active');
   if(modal != null){
       modal.classList.toggle('is-active');
@@ -261,6 +324,7 @@ function addVariableGlobalModal(){
 }
 
 function addFunctionModal(){
+  $("#params").empty()
   var modal = document.getElementById('addFunctionModal');
   modal.classList.toggle('is-active');
 }
@@ -278,12 +342,62 @@ function addVectorLocalModal(){
   modal.classList.toggle('is-active');
 }
 
-function addOperacionLocalModal(){
+function addOperacionLocalModal(func_name){
   var modal = document.getElementById('addOperacionLocalModal');
   modal.classList.toggle('is-active');
+  is_inside = func_name
+  //var element = document.getElementById("op");
+  //element.setAttribute("id", "if"+ func_name + count)
 }
 
 function addCondicionModal(){
-  var modal = document.getElementById('addOperacionLocalModal');
-  modal.classList.toggle('is-active');
+
 }
+
+/* -------- Funciones que se llaman en tiepo real -------- */
+
+function addParamsToModal(){
+  paramCounter = paramCounter + 1;
+  var paramDiv = '<h5 class="subtitle is-5">Selecciona el tipo del parametro:</h5>'+
+  '<div class="control">'+
+    '<label class="radio">'+
+      '<input id="' + ("paramInt" + paramCounter) + '" class="radio-param"type="radio" name="type' + paramCounter +'">'+
+      'Entero'+
+    '</label>'+
+    '<label class="radio">'+
+      '<input id="' + ("paramFloat" + paramCounter) + '" class="radio-param"type="radio" name="type' + paramCounter +'">'+
+      'Flotante'+
+    '</label>'+
+    '<label class="radio">'+
+      '<input id="' + ("paramBool" + paramCounter) + '" class="radio-param"type="radio" name="type' + paramCounter +'">'+
+      'Booleano'+
+    '</label>'+
+    '<label class="radio">'+
+      '<input id="' + ("paramString" + paramCounter) + '" class="radio-param"type="radio" name="type' + paramCounter +'">'+
+      'Frase'+
+    '</label>'+
+  '</div>'+
+  '<br>'+
+  '<div class="columns">'+
+    '<div class="column is-5">'+
+      '<h5 class="subtitle is-5" style="margin-top: 5px;">Nombre del parametro:</h5>'+
+    '</div>'+
+    '<div class="column" style="margin-left: -50px;">'+
+      '<input id="' + ("paramName" + paramCounter) + '" class="input" type="text" placeholder="Nombre">'+
+    '</div>'+
+  '</div>"'
+  $(paramDiv).appendTo("#params")
+  return ;
+}
+
+// function clearTexbox(){
+//   $("#textbox").val("")
+// }
+
+/* -------- Funciones extras -------- */
+
+// let dropdown = document.querySelector('.dropdown');
+// dropdown.addEventListener('click', function(event) {
+//     event.stopPropagation();
+//     dropdown.classList.toggle('is-active');
+// });
